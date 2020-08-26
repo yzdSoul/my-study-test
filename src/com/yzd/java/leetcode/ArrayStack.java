a09e1da578c24811ae1a9a94f8ccdbf07765b051
yzdSoul <yzdsoul@gmail.com>
2020-08-26 18:05:21 +0800

    用数组和链表实现栈

--- src/com/yzd/java/leetcode/ArrayStack.java
+++ src/com/yzd/java/leetcode/ArrayStack.java
@@ -21,12 +21,22 @@ public class ArrayStack<Item> implements MyStack<Item>{
 
     @Override
     public Item pop() throws Exception {
-        return null;
+        if (isEmpty()){
+            throw new Exception("stack is empty");
+        }
+
+        Item item = items[--N];
+
+        //check();
+
+        items[N] = null;
+
+        return item;
     }
 
     @Override
     public boolean isEmpty() {
-        return false;
+        return N == 0;
     }
 
     @Override
@@ -36,6 +46,36 @@ public class ArrayStack<Item> implements MyStack<Item>{
 
     @Override
     public Iterator iterator() {
-        return null;
+        return new Iterator<Item>() {
+
+            private int i = N;
+            @Override
+            public boolean hasNext() {
+                return i > 0;
+            }
+
+            @Override
+            public Item next() {
+                return items[--i];
+            }
+        };
+    }
+
+    private void check(){
+        if (N >= items.length){
+            resize(2 * items.length);
+        } else if (N > 0 && N < items.length / 4 ){
+            resize(items.length / 2);
+        }
+    }
+
+    private void resize(int size) {
+        Item[] temp = (Item[]) new Object[size];
+
+        for (int i = 0; i < N; i++) {
+            temp[i] = items[i];
+        }
+
+        items = temp;
     }
 }
---
+++ src/com/yzd/java/leetcode/ListStack.java
@@ -0,0 +1,75 @@
+package com.yzd.java.leetcode;
+
+import java.util.Iterator;
+
+/**
+ * Created by yzd on 2020/8/26
+ *
+ * 需要使用链表的头插法来实现，因为头插法中最后压入栈的元素在链表的开头，它的 next 指针指向前一个压入栈的元素，
+ * 在弹出元素时就可以通过 next 指针遍历到前一个压入栈的元素从而让这个元素成为新的栈顶元素。
+ */
+public class ListStack<Item>implements MyStack<Item> {
+
+    private Node top = null;
+    private int N = 0;
+    private class Node {
+        Item item;
+        Node next;
+    }
+
+    @Override
+    public MyStack<Item> push(Item item) {
+        Node newTop = new Node();
+
+        newTop.item = item;
+        newTop.next = top;
+
+        top = newTop;
+
+        N++;
+        return this;
+    }
+
+    @Override
+    public Item pop() throws Exception {
+
+        if (isEmpty()){
+            throw new Exception("stack is empty");
+        }
+
+        Item item = top.item;
+
+        top = top.next;
+        N--;
+        return item;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return N == 0;
+    }
+
+    @Override
+    public int size() {
+        return N;
+    }
+
+    @Override
+    public Iterator iterator() {
+        return new Iterator() {
+            private Node cur = top;
+
+            @Override
+            public boolean hasNext() {
+                return cur != null;
+            }
+
+            @Override
+            public Item next() {
+                Item item = cur.item;
+                cur = cur.next;
+                return item;
+            }
+        };
+    }
+}
