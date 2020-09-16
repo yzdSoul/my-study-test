65c8d30f157d1f7c37e08a4568dcf9580ff1fcf5
yzdSoul <yzdsoul@gmail.com>
2020-09-16 18:02:01 +0800

    commit

--- src/com/yzd/java/fucking_algorithm/Permute.java
+++ src/com/yzd/java/fucking_algorithm/Permute.java
@@ -12,28 +12,31 @@ public class Permute {
     List<List<Integer>> res = new LinkedList<>();
 
     public List<List<Integer>> permute(int[] nums) {
-
+        //记录路径
         LinkedList<Integer> track = new LinkedList<>();
         backTrack(nums,track);
         return res;
     }
-
+    //路径记录在track中
+    //选择列表：muns 中不存在track的元素
+    //结束条件：nums 中的元素全在track中
     private void backTrack(int[] nums, LinkedList<Integer> track) {
-
+        //触发结束条件
         if (track.size() == nums.length) {
             res.add(new LinkedList<>(track));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
+            //排除不合法的选择
             if (track.contains(nums[i])) {
                 continue;
             }
-
+            //做选择
             track.add(nums[i]);
-
+            //下一层决策树
             backTrack(nums, track);
-
+            // 取消选择
             track.removeLast();
         }
     }
