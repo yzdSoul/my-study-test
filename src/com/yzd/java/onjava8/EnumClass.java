efb32e85e31af364dc210ac0f135789bb8bbd4ef
yzdSoul <yzdsoul@gmail.com>
2020-09-29 19:31:21 +0800

    commit

---
+++ src/com/yzd/java/onjava8/EnumClass.java
@@ -0,0 +1,29 @@
+package com.yzd.java.onjava8;
+
+/**
+ * Created by yzd on 2020/9/29
+ */
+enum Shrubbery { GROUND, CRAWLING, HANGING }
+public class EnumClass {
+    public static void main(String[] args) {
+        for(Shrubbery s : Shrubbery.values()) {
+            System.out.println(
+                    s + " ordinal: " + s.ordinal());
+            System.out.print(
+                    s.compareTo(Shrubbery.CRAWLING) + " ");
+            System.out.print(
+                    s.equals(Shrubbery.CRAWLING) + " ");
+            System.out.println(s == Shrubbery.CRAWLING);
+            System.out.println(s.getDeclaringClass());
+            System.out.println(s.name());
+            System.out.println("********************");
+        }
+// Produce an enum value from a String name:
+        for(String s :
+                "HANGING CRAWLING GROUND".split(" ")) {
+            Shrubbery shrub =
+                    Enum.valueOf(Shrubbery.class, s);
+            System.out.println(shrub);
+        }
+    }
+}
