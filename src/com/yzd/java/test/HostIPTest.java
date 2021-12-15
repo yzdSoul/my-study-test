036ee02450e484287626f2062b793d9ce4e5ae6a
yzdSoul <yzdsoul@gmail.com>
2021-12-15 18:02:27 +0800

    差分数组

---
+++ src/com/yzd/java/test/HostIPTest.java
@@ -0,0 +1,44 @@
+package com.yzd.java.test;
+
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.util.Enumeration;
+
+/**
+ * Created by yzd on 2021/12/15
+ */
+public class HostIPTest {
+
+    public static String getHostIp() {
+
+        String sIP = "";
+        InetAddress ip = null;
+        try {
+            boolean bFindIP = false;
+            Enumeration<NetworkInterface> netInterfaces = NetworkInterface.getNetworkInterfaces();
+            while (netInterfaces.hasMoreElements()) {
+                if (bFindIP)
+                    break;
+                NetworkInterface ni = netInterfaces.nextElement();
+                Enumeration<InetAddress> ips = ni.getInetAddresses();
+                while (ips.hasMoreElements()) {
+                    ip = ips.nextElement();
+                    if (!ip.isLoopbackAddress()
+                            && ip.getHostAddress().matches("(\\d{1,3}\\.){3}\\d{1,3}")) {
+                        bFindIP = true;
+                        break;
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        if (null != ip)
+            sIP = ip.getHostAddress();
+        return sIP;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(HostIPTest.getHostIp());
+    }
+}
