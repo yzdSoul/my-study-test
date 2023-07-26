0d729da4660818e8f8c838d40f9fb8b5fc8fd1e5
yzdSoul <zhendong.yang@9icaishi.net>
2023-07-26 16:48:10 +0800

    提交代码

--- src/main/java/com/yzd/java/fucking_algorithm/array/BinarySearch.java
+++ src/main/java/com/yzd/java/fucking_algorithm/array/BinarySearch.java
@@ -1,5 +1,7 @@
 package com.yzd.java.fucking_algorithm.array;
 
+import java.util.Arrays;
+
 /**
  * Created by yzd on 2021/12/16
  *
@@ -79,5 +81,74 @@ public class BinarySearch {
         }
         if(left == 0) return -1;
         return nums[left - 1] == target ? (left - 1) : -1;
+    }
+//    双指针 时间复杂度 O(n)
+    public int[] getResult(int[] nums, int target){
+        int[] result = new int[]{-1,-1};
+        if (nums.length == 0) return result;
+        int left = 0, right = nums.length - 1;
+        // 搜索区间为 [left,right]
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[left] == target) {
+                result[0] = left;
+            } else {
+                left++;
+            }
+            if (nums[right] == target) {
+                result[1] = right;
+            } else {
+                right--;
+            }
+            if (result[0] != -1 && result[1] != -1) {
+                break;
+            }
+        }
+        return result;
+    }
+//    二分查找 时间复杂度 O(logn)
+    public int[] getResult2(int[] nums,int target){
+        return new int[]{getLeft(nums,target),getRight(nums,target)};
+    }
+    public int getLeft(int[] nums,int target){
+        if(nums.length == 0) return -1;
+        int left = 0,right = nums.length - 1;
+        while (left <= right){
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < target){
+                left = mid + 1;
+            } else if (nums[mid] >target) {
+                right = mid - 1;
+            } else if (nums[mid] == target) {
+                right = mid - 1;
+            }
+        }
+        if (left >= nums.length || nums[left] != target){
+            return -1;
+        }
+        return left;
+    }
+    public int getRight(int[] nums,int target){
+        if(nums.length == 0) return -1;
+        int left = 0, right = nums.length - 1;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
+                left = mid + 1;
+            } else if (nums[mid] > target) {
+                right = mid - 1;
+            }
+        }
+        return nums[left - 1] == target ? (left - 1) : -1;
+    }
+    public static void main(String[] args) {
+        int[] nums = new int[]{5,7,7,7,8,8,8,8,10};
+        BinarySearch binarySearch = new BinarySearch();
+//        int[] ints = binarySearch.getResult(nums, 8);
+        int[] ints = binarySearch.getResult2(nums, 8);
+//        int[] ints = binarySearch.searchRange(nums, 8);
+        System.out.println(Arrays.toString(ints));
+//        System.out.println(binarySearch.getLeft(nums,8));
     }
 }
